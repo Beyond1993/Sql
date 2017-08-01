02e0c18a8a7f4e2f2cb4f752175c216880dfa24b
Wayne Feng <wfeng@wfeng-mbp13-qk02c.dyn.pinadmin.com>
2017-08-01 11:20:38 -0700

    add join

---
+++ Join_Operation.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS person;
+CREATE TABLE person(
+    name  varchar(20),
+    age   int,
+    sex   varchar(20)
+);
+
+INSERT INTO person VALUES ( 'mike',  34, 'male');
+INSERT INTO person VALUES ( 'wayne', 24, 'male');
+INSERT INTO person VALUES ( 'mary',  24, 'female');
+
+DROP TABLE IF EXISTS employer;
+CREATE TABLE employer (
+    name   varchar(20),
+    title  varchar(20),
+    salary int,
+    age   int 
+);
+
+INSERT INTO employer VALUES ( 'mike','SDE', 1000, 34);
+INSERT INTO employer VALUES ( 'bob', 'QA',  2000, 24);
+INSERT INTO employer VALUES ( 'mary','HR',  500, 24);
+
